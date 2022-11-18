727e9ab3644ee8ba84e311fe6e1f5eec2a331fe8
AnuMonachan <anumonachan9953@gmail.com>
2022-11-18 11:14:05 +0530

    login & logout

---
+++ stores/authContext.js
@@ -0,0 +1,51 @@
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget";
+
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false,
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+      netlifyIdentity.close();
+      console.log("logged in");
+    });
+
+    netlifyIdentity.on("logout", (user) => {
+      setUser(null);
+      console.log("logged out");
+    });
+
+    //init netlify identity connection
+    netlifyIdentity.init();
+
+    return () => {
+      netlifyIdentity.off("login");
+      netlifyIdentity.off("logout");
+    };
+  }, []);
+
+  const login = () => {
+    netlifyIdentity.open();
+  };
+
+  const logout = () => {
+    netlifyIdentity.logout();
+  };
+
+  const context = { user, login, logout };
+  // {user, login} can be written like this also instead of {user: user, login: login}
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
